d14ae3e7c7b088a4dfc18afa1c7c6e1f888b372f
Ivan091 <kornasevich@gmail.com>
2021-06-12 22:18:12 +0300

    Swing almost done

--- swing/src/main/java/alive/ApplicationSwing.java
+++ swing/src/main/java/alive/ApplicationSwing.java
@@ -1,14 +1,17 @@
 package alive;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 public class ApplicationSwing {
 
     public static void main(String[] args) {
-        var context = SpringApplication.run(ApplicationSwing.class, args);
+        var context = new SpringApplicationBuilder(ApplicationSwing.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
         var view = context.getBean(View.class);
     }
 }
--- swing/src/main/java/alive/View.java
+++ swing/src/main/java/alive/View.java
@@ -1,10 +1,51 @@
 package alive;
 
+import alive.simulation.SimulationField;
 import org.springframework.stereotype.Component;
+import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 
 @Component
 public class View extends Canvas {
 
+    private final SimulationField simulationField;
+
+    public View(SimulationField simulationField) {
+        this.simulationField = simulationField;
+    }
+
+    private void startGame() {
+        JFrame frame = new JFrame();
+        var c = new Field();
+        frame.add(c);
+        c.setBackground(Color.BLACK);
+        frame.setBackground(Color.BLACK);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.pack();
+        frame.setVisible(true);
+        c.startGame();
+        simulation.start();
+        Executors.newScheduledThreadPool(4).scheduleAtFixedRate(() ->
+                paint(getGraphics()), 0, 32, TimeUnit.MILLISECONDS);
+        Executors.newScheduledThreadPool(4).scheduleWithFixedDelay(() ->
+                IntStream.range(0, 100000).forEach(i -> simulation.nextMove()), 1000, 1, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        var ent = simulation.getField().cellsMatrix().getEntities();
+        var yLen = ent.length;
+        var xLen = ent[0].length;
+        for (var i = 0; i < yLen; i++) {
+            for (var j = 0; j < xLen; j++) {
+                var entity = ent[i][j];
+                g.setColor(entity.color());
+                g.fillRect(j * 6, i * 6, 6, 6);
+            }
+        }
+    }
 }
